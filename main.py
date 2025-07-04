f14aef7b7fdab42da49e90636e93ddee321cd6d3
sri ram v <sriram2.10.2002@gmail.com>
2025-07-04 12:24:38 +0530

    General script file

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("Hello world")
+class Human:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    
+    def talk(self):
+        print(f"{self.name} is talking")
+        
+    def getData(self):
+        return f"{self.name} is {self.age} years old"
+    
+    
+class Student(Human):
+    def __init__(self, name, age,year):
+        super().__init__(name, age)
+        self.year = year
+    
+    def getYear(self):
+        print(f"{self.name} is graduated in the year of {self.year}")
+
+h1=Human("Luffy",21)
+h2=Human("sanji",23)
+h3=Human("zoro",23)
+
+h4=Student("Nami",21,2024)
+
+h1.getYear()
